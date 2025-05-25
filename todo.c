5f29e562cefc8415db952f91e43cd3c21b781346
Per Rawdin <perrawd@gmail.com>
2025-05-25 18:46:49 +0200

    Add promtSelection function for displayMenu

--- todo.c
+++ todo.c
@@ -3,29 +3,31 @@
 
 bool active = true;
 
-void displayMenu(bool *active) {
-  int selection;
-
-  printf("1. Add task\n2. List tasks\n3. Exit program\n");
-  scanf("%d", &selection);
-  while (getchar() != '\n');
-
+void promtSelection(int selection) {
   switch (selection) {
-    case 1:
-      printf("You've selected 1.\n");
-      break;
-    case 2:
-      printf("You've selected 2.\n");
-      break;
-    case 3:
-      *active = false;
-      break;
-    default:
-      printf("Invalid option.\n");
+  case 1:
+    printf("You've selected 1.\n");
+    break;
+  case 2:
+    printf("You've selected 2.\n");
+    break;
+  case 3:
+    active = false;
+    break;
+  default:
+    printf("Invalid option.\n");
   }
 }
 
+void displayMenu() {
+  int selection;
+  printf("1. Add task\n2. List tasks\n3. Exit program\n");
+  scanf("%d", &selection);
+  while (getchar() != '\n');
+  promtSelection(selection);
+}
+
 int main(void) {
-  while (active) displayMenu(&active);
+  while (active) displayMenu();
   return 0;
 }
