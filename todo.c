20631d19f7478aafbe9a3f41ee1441b4a7a91c49
Per Rawdin <perrawd@gmail.com>
2025-05-25 19:06:00 +0200

    Add addTask function

--- todo.c
+++ todo.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <string.h>
 
 bool active = true;
+int listItem = 0;
 
 struct Task {
   char title[50];
@@ -11,10 +13,20 @@ struct Task listOfTasks[10] = {
     {"", ""}
 };
 
-void promtSelection(int selection) {
+void addTask() {
+  int listItemIndex = listItem;
+  printf("Enter task title: ");
+  fgets(listOfTasks[listItemIndex].title, sizeof(listOfTasks[0].title), stdin);
+  // Remove the newline character if present
+  listOfTasks[listItemIndex].title[strlen(listOfTasks[listItemIndex].title) - 1] = '\0';
+  listItem += 1;
+  return;
+}
+
+void promptSelection(int selection) {
   switch (selection) {
   case 1:
-    printf("You've selected 1.\n");
+    addTask();
     break;
   case 2:
     printf("You've selected 2.\n");
@@ -32,7 +44,7 @@ void displayMenu() {
   printf("1. Add task\n2. List tasks\n3. Exit program\n");
   scanf("%d", &selection);
   while (getchar() != '\n');
-  promtSelection(selection);
+  promptSelection(selection);
 }
 
 int main(void) {
