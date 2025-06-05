c002030d436e9cf9936b9e2dd956e6be129ecd48
Per Rawdin <perrawd@gmail.com>
2025-06-05 21:29:34 +0200

    Reorder functions and add top-level declarations

--- todo.c
+++ todo.c
@@ -5,32 +5,25 @@
 bool active = true;
 int listItem = 0;
 
-struct Task {
-  char title[50];
-  char description[50];
-};
-struct Task listOfTasks[10] = {
-    {"", ""}
-};
+struct Task { char title[50]; char description[50]; };
+struct Task listOfTasks[10] = {{"", ""}};
 
-void displayListOfTask() {
-    int i;
-    for (i = 0; i < 10; i++) {
-      printf("Task title: %s\n", listOfTasks[i].title);
-      printf("Task description: %s\n", listOfTasks[i].description);
-      printf("************************\n");
-    }
-    return;
+void displayMenu();
+void promptSelection(int selection);
+void addTask();
+void displayListOfTask();
+
+int main(void) {
+  while (active) displayMenu();
+  return 0;
 }
 
-void addTask() {
-  int listItemIndex = listItem;
-  printf("Enter task title: ");
-  fgets(listOfTasks[listItemIndex].title, sizeof(listOfTasks[0].title), stdin);
-  // Remove the newline character if present
-  listOfTasks[listItemIndex].title[strlen(listOfTasks[listItemIndex].title) - 1] = '\0';
-  listItem += 1;
-  return;
+void displayMenu() {
+  int selection;
+  printf("1. Add task\n2. List tasks\n3. Exit program\n");
+  scanf("%d", &selection);
+  while (getchar() != '\n');
+  promptSelection(selection);
 }
 
 void promptSelection(int selection) {
@@ -49,15 +42,22 @@ void promptSelection(int selection) {
   }
 }
 
-void displayMenu() {
-  int selection;
-  printf("1. Add task\n2. List tasks\n3. Exit program\n");
-  scanf("%d", &selection);
-  while (getchar() != '\n');
-  promptSelection(selection);
+void addTask() {
+  int listItemIndex = listItem;
+  printf("Enter task title: ");
+  fgets(listOfTasks[listItemIndex].title, sizeof(listOfTasks[0].title), stdin);
+  // Remove the newline character if present
+  listOfTasks[listItemIndex].title[strlen(listOfTasks[listItemIndex].title) - 1] = '\0';
+  listItem += 1;
+  return;
 }
 
-int main(void) {
-  while (active) displayMenu();
-  return 0;
+void displayListOfTask() {
+  int i;
+  for (i = 0; i < 10; i++) {
+    printf("Task title: %s\n", listOfTasks[i].title);
+    printf("Task description: %s\n", listOfTasks[i].description);
+    printf("************************\n");
+  }
+  return;
 }
