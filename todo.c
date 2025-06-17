98d07beab5316b05e54eefa9b06a2c6528f843eb
Per Rawdin <perrawd@gmail.com>
2025-06-17 10:16:27 +0200

    Refactor getTaskIndex and getTaskIndex

--- todo.c
+++ todo.c
@@ -102,19 +102,18 @@ void editTask() {
 
 int getTaskIndex() {
   int taskIndex = 0;
-  while (taskIndex == 0) {
-    promptForTaskIndex(&taskIndex);
-    if (taskIndex == 0) displayListOfTask();
-  }
-  taskIndex -= 1;
-  return taskIndex;
+  promptForTaskIndex(&taskIndex);
+  return taskIndex - 1;
 }
 
 void promptForTaskIndex(int *taskIndex) {
-  clearScreen();
-  printf("(Enter 0 for list of tasks)\nEnter the index of the task you want to edit: ");
-  scanf("%d", taskIndex);
-  while (getchar() != '\n');
+  while (taskIndex == 0) {
+    clearScreen();
+    printf("(Enter 0 for list of tasks)\nEnter the index of the task you want to edit: ");
+    scanf("%d", taskIndex);
+    while (getchar() != '\n');
+    if (taskIndex == 0) displayListOfTask();
+  }
 }
 
 int getEditType() {
